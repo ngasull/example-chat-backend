768dd77b0d11a999522323a6f0fc78c2d58508ec
blint <nicolas@gasull.net>
2015-05-17 19:48:58 +0200

    Implemented minimalistic real time IO with spec

---
+++ src/_spec/ChatBackend.spec.js
@@ -0,0 +1,166 @@
+var should = require('should')
+var request = require('supertest')
+
+var ChatBackend = require('../express')
+
+var backend = new ChatBackend()
+var app = backend.app
+backend.setupApiRoutes()
+
+describe('ChatBackend', function () {
+
+    describe('/api/message', function () {
+
+        var basePath = '/api/message'
+        var firstMessage, updatedFirstMessage, secondMessage, thirdMessage
+
+        it('GET / should return an empty list', function (done) {
+            request(app)
+                .get(basePath)
+                .set('Accept', 'application/json')
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .end(function (err, res) {
+                    should(err).be.null
+                    res.body.should.eql([])
+                    done()
+                })
+        })
+
+        it('POST / should add content and send what is persisted', function (done) {
+            request(app)
+                .post(basePath)
+                .set('Accept', 'application/json')
+                .send({
+                    author: 'blint',
+                    text: 'Hello API',
+                    dummyParam: 'foo'
+                })
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .end(function (err, res) {
+                    should(err).be.null
+                    firstMessage = res.body
+                    firstMessage.should.have.property('id')
+                    firstMessage.should.have.property('author', 'blint')
+                    firstMessage.should.have.property('text', 'Hello API')
+                    firstMessage.should.not.have.property('dummyParam')
+                    done()
+                })
+        })
+
+        it('GET /id should return a previously added message by its id', function (done) {
+            request(app)
+                .get(basePath + '/' + firstMessage.id)
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .end(function (err, res) {
+                    should(err).be.null
+                    res.body.should.eql(firstMessage)
+                    done()
+                })
+        })
+
+        it('GET /id should end in error for unknown ids', function (done) {
+            request(app)
+                .get(basePath + '/42')
+                .expect(500)
+                .end(function (err) {
+                    should(err).be.null
+                    done()
+                })
+        })
+
+        it('PUT / should modify some content', function (done) {
+            request(app)
+                .put(basePath)
+                .set('Accept', 'application/json')
+                .send({
+                    id: firstMessage.id,
+                    author: firstMessage.author,
+                    text: 'Hi API!',
+                    dummyParam: 'foo'
+                })
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .end(function (err, res) {
+                    should(err).be.null
+
+                    updatedFirstMessage = res.body
+                    updatedFirstMessage.should.have.property('id', firstMessage.id)
+                    updatedFirstMessage.should.have.property('author', firstMessage.author)
+                    updatedFirstMessage.should.have.property('text', 'Hi API!')
+                    updatedFirstMessage.should.not.have.property('dummyParam')
+
+                    done()
+                })
+        })
+
+        it('POST / should add a second message', function (done) {
+            request(app)
+                .post(basePath)
+                .set('Accept', 'application/json')
+                .send({
+                    author: 'Komo',
+                    text: 'Welcome'
+                })
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .end(function (err, res) {
+                    should(err).be.null
+                    secondMessage = res.body
+                    done()
+                })
+        })
+
+        it('POST / should add a third message', function (done) {
+            request(app)
+                .post(basePath)
+                .set('Accept', 'application/json')
+                .send({
+                    author: 'Bubux',
+                    text: 'Hey guys'
+                })
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .end(function (err, res) {
+                    should(err).be.null
+                    thirdMessage = res.body
+                    done()
+                })
+        })
+
+        it('DELETE / should delete second message', function (done) {
+            request(app)
+                .delete(basePath + '/' + secondMessage.id)
+                .expect(200)
+                .end(function (err) {
+                    should(err).be.null
+                    done()
+                })
+        })
+
+        it('GET /id should fail returning second message', function (done) {
+            request(app)
+                .get(basePath + '/' + secondMessage.id)
+                .expect(500)
+                .end(function (err) {
+                    should(err).be.null
+                    done()
+                })
+        })
+
+        it('GET / should return the list of messages with all their modifications', function (done) {
+            request(app)
+                .get(basePath)
+                .set('Accept', 'application/json')
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .end(function (err, res) {
+                    should(err).be.null
+                    res.body.should.eql([updatedFirstMessage, thirdMessage])
+                    done()
+                })
+        })
+    })
+})
--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var routeApi = require('./lib/routeApi')
+var MessageApi = require('./lib/api/message')
+
 /**
- * Created by blint on 16/05/15.
+ * Creates the chat express backend.
+ * ChatBackend.app can be used to setup routes before calling ChatBackend.setupApiRoutes
+ * @constructor
  */
+function ChatBackend() {
+
+    this.app = express()
+    this.messageApi = new MessageApi()
+
+    this.app.use(bodyParser())
+}
+
+/**
+ * Configures routes for the API. Top level routes may have been configured before calling this.
+ */
+ChatBackend.prototype.setupApiRoutes = function () {
+
+    this.app.use('/api/message', routeApi(this.messageApi))
+}
+
+module.exports = ChatBackend
